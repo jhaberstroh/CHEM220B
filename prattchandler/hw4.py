4cc675ab589bda53d204e3f0ae72936b183f1e6f
John Haberstroh <jhaberstroh@berkeley.edu>
2015-02-27 14:04:52 -0800

    Refined features of hw4.py

--- prattchandler/hw4.py
+++ prattchandler/hw4.py
@@ -160,60 +160,68 @@ def compute_gr(sig_A_A = 2.7, bplot = False):
     for i, r_i in enumerate(r_gr):
         integrand_k = h_AA_k * k_A * np.sin(k_A * r_i)
         h_AA_r[i] = np.sum((integrand_k[1:] + integrand_k[:-1]) / 2. * dk_A) / (2 * np.pi **2 * r_i)
-    h_AA_r[r_gr < sig_A_A] = 0
+    h_AA_r[r_gr < sig_A_A] = -1
 
     return r_gr, g_AW_r + g_AW_r2 + 1, h_AA_r + 1
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-plot", action="store_true")
+    parser.add_argument("-gaw", action="store_true")
+    parser.add_argument("-dmu", action="store_true")
+    parser.add_argument("-gaa", action="store_true")
     args = parser.parse_args()
     
-    l = []
-    for sig_A in np.linspace(3, 5, 3):
-        r, g_AW_r, g_AA_r = compute_gr(sig_A)
-        R = (sig_A + 2.7) / 2.
-        plt.plot(r - R, g_AW_r)
-        l.append("sig={}".format(sig_A))
-    plt.title("g_AW(r) at different radii")
-    plt.legend(l)
-    plt.show()
-    
-    N_R = 40
-    R_max = 10.0
-    sig_A_list = np.linspace(0, (R_max - 2.7), N_R)
-    R_list = (sig_A_list + 2.7) / 2.0
-    g_AW = np.zeros(N_R)
-    for i, sig_i in enumerate(sig_A_list):
-        r, gr, _ = compute_gr(sig_i)
-        subset = (r > R_list[i])
-        gr = gr[subset]
-        r  = r [subset]
-        r_min = r[0]
-        # Use the magnitude of the slope
-        g_slope = abs( (gr[1] - gr[0])/(r[1] - r[0]) )
-        g_peak = gr[0] +  (r[0] - R_list[i]) * g_slope
-        g_AW[i] = g_peak
-    integrand = g_AW * 4 * np.pi * np.square(R_list)
-    factor = .596 * .033
-    delta_mu = np.cumsum((integrand[1:] + integrand[:-1]) / 2. * np.diff(R_list))
-    delta_mu *= factor
-    delta_mu = np.insert(delta_mu, 0, 0.)
-    plt.plot(sig_A_list, delta_mu)
-    plt.title("Solvation free energy for hydrophobe from Pratt-Chandler")
-    plt.ylabel("Delta mu, KCal/mol")
-    plt.xlabel("Solute diameter, Angstrom")
-    plt.show()
+    if args.gaw:
+        l = []
+        for sig_A in np.linspace(3, 5, 3):
+            r, g_AW_r, g_AA_r = compute_gr(sig_A)
+            R = (sig_A + 2.7) / 2.
+            plt.plot(r - R, g_AW_r)
+            l.append("sig={}".format(sig_A))
+        plt.title("g_AW(r) at different radii")
+        plt.xlabel("Solute diameter, Angstrom")
+        plt.legend(l)
+        plt.show()
+   
+    if args.dmu:
+        N_R = 40
+        R_max = 10.0
+        sig_A_list = np.linspace(0, (R_max - 2.7), N_R)
+        R_list = (sig_A_list + 2.7) / 2.0
+        g_AW = np.zeros(N_R)
+        for i, sig_i in enumerate(sig_A_list):
+            r, gr, _ = compute_gr(sig_i)
+            subset = (r > R_list[i])
+            gr = gr[subset]
+            r  = r [subset]
+            r_min = r[0]
+            # Use the magnitude of the slope
+            g_slope = abs( (gr[1] - gr[0])/(r[1] - r[0]) )
+            g_peak = gr[0] +  (r[0] - R_list[i]) * g_slope
+            g_AW[i] = g_peak
+        integrand = g_AW * 4 * np.pi * np.square(R_list)
+        factor = .596 * .033
+        delta_mu = np.cumsum((integrand[1:] + integrand[:-1]) / 2. * np.diff(R_list))
+        delta_mu *= factor
+        delta_mu = np.insert(delta_mu, 0, 0.)
+        plt.plot(sig_A_list, delta_mu)
+        plt.title("Solvation free energy for hydrophobe from Pratt-Chandler")
+        plt.ylabel("Delta mu, KCal/mol")
+        plt.xlabel("Solute diameter, Angstrom")
+        plt.show()
 
-    l = []
-    for sig_A in np.linspace(3, 5, 3):
-        r, g_AW_r, g_AA_r = compute_gr(sig_A)
-        R = (sig_A + 2.7) / 2.
-        plt.plot(r - sig_A, g_AA_r)
-        l.append("sig={}".format(sig_A))
-    plt.title("g_AA(r) at different radii")
-    plt.legend(l)
-    plt.show()
+    if args.gaa:
+        l = []
+        for sig_A in np.linspace(1, 7, 3):
+            r, g_AW_r, g_AA_r = compute_gr(sig_A)
+            R = (sig_A + 2.7) / 2.
+            plt.plot(r - sig_A, g_AA_r)
+            l.append("sig={}".format(sig_A))
+        plt.title("g_AA(r) at different radii")
+        plt.xlabel("Solute Diameter, Angstrom")
+        plt.legend(l)
+        plt.show()
 
         
     

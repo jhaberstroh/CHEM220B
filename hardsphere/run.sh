6f58df481c1798b99609f6b1dcc1ad3e1de7e01c
John Haberstroh <jhaberstroh@berkeley.edu>
2015-03-05 14:39:56 -0800

    Updated environment variables

--- hardsphere/run.sh
+++ hardsphere/run.sh
@@ -2,21 +2,23 @@
 # Run with HW#-SIM or -DATA to run code for the corresponding homework assignment
 set -o errexit
 
-#SAVEDIR=/home/jhaberstroh/Dropbox/Physics/CHEM220B
-if [ -z ${SAVEDIR+x} ]; then
-    SAVEDIR=images
+if [ -z ${CHEM220_IMGDIR+x} ]; then
+    CHEM220_IMGDIR=images
 fi
-if [ ! -e $SAVEDIR ]; then
-    mkdir $SAVEDIR
+if [ ! -e $CHEM220_IMGDIR ]; then
+    mkdir $CHEM220_IMGDIR
+fi
+
+if [ -z ${CHEM220_DATDIR+x} ]; then
+    CHEM220_IMGDIR=data
+fi
+if [ ! -e $CHEM220_DATDIR ]; then
+    mkdir $CHEM220_DATDIR
 fi
 
 if [ -z ${1+x} ]; then
     echo "Requires argument HW#-SIM or HW#-DATA to run"
     exit
-fi
-
-if [ ! -e data ]; then
-    mkdir data
 fi
 
 if [ $1 == "TEST-MC" ]; then
@@ -41,24 +43,24 @@ if [ $1 == "HW1-SIM" ]; then
     echo "Submitted one hardsphere-xyz, one hardsphere-sol and three" \
    " hardsphere-lg tasks"
     gcc hardsphere.cc -lstdc++ -o hardsphere-xyz -DXYZOUT
-    ./hardsphere-xyz -nsteq 400 -step_size .7 > data/atoms.xyz & 
+    ./hardsphere-xyz -nsteq 400 -step_size .7 > $CHEM220_DATDIR/atoms.xyz & 
     
     gcc hardsphere.cc -lstdc++ -o hardsphere-sol -DSMALLSPHERE
-    ./hardsphere-sol -nsteq 500 -step_size .7 > data/small.csv &
+    ./hardsphere-sol -nsteq 500 -step_size .7 > $CHEM220_DATDIR/small.csv &
     
     gcc hardsphere.cc -lstdc++ -o hardsphere-lg -DLARGESPHERE
-    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 1.0 > data/large-2.0.csv &
-    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 1.5 > data/large-3.0.csv &
-    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 2.0 > data/large-4.0.csv &
+    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 1.0 > $CHEM220_DATDIR/large-2.0.csv &
+    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 1.5 > $CHEM220_DATDIR/large-3.0.csv &
+    ./hardsphere-lg -nsteq 500 -step_size .7 -probe 2.0 > $CHEM220_DATDIR/large-4.0.csv &
 fi
 
 if [ $1 == "HW1-DATA" ]; then
-    echo "Saving images to $SAVEDIR/hw1"
-    if [ ! -e $SAVEDIR/hw1 ]; then
-        mkdir $SAVEDIR/hw1
+    echo "Saving images to $CHEM220_IMGDIR/hw1"
+    if [ ! -e $CHEM220_IMGDIR/hw1 ]; then
+        mkdir $CHEM220_IMGDIR/hw1
     fi
-    python script/plotxyz.py -dir data -save $SAVEDIR/hw1
-    python script/hist.py -dir data -save $SAVEDIR/hw1
+    python script/plotxyz.py -dir $CHEM220_DATDIR -save $CHEM220_IMGDIR/hw1
+    python script/hist.py -dir $CHEM220_DATDIR -save $CHEM220_IMGDIR/hw1
 fi
 
 # =============================================================================
@@ -68,15 +70,15 @@ if [ $1 == "HW2-SIM" ]; then
     echo "Submitted one hardsphere-fourier task"
     gcc hardsphere.cc -lstdc++ -o hardsphere-fourier -DFOURIER
     ./hardsphere-fourier -nsteq 500 -nstmc 10000 -nstfourier 100 -step_size .7 \
-        -maxfouriernum 100 > data/fourier.csv &
+        -maxfouriernum 100 > $CHEM220_DATDIR/fourier.csv &
 fi
 
 if [ $1 == "HW2-DATA" ]; then
-    echo "Saving images to $SAVEDIR/hw2"
-    if [ ! -e $SAVEDIR/hw2 ]; then
-        mkdir $SAVEDIR/hw2
+    echo "Saving images to $CHEM220_IMGDIR/hw2"
+    if [ ! -e $CHEM220_IMGDIR/hw2 ]; then
+        mkdir $CHEM220_IMGDIR/hw2
     fi
-    python script/fourier.py -file data/fourier.csv -save $SAVEDIR/hw2
+    python script/fourier.py -file $CHEM220_DATDIR/fourier.csv -save $CHEM220_IMGDIR/hw2
 fi
 
 # =============================================================================
@@ -85,30 +87,30 @@ fi
 if [ $1 == "HW3-SIM" ]; then
     gcc hardsphere.cc -lstdc++ -o hardsphere-gr -DGR
     if [ $? = 0 ]; then
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  .05 -density  .9 > data/gr_9.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .1 -density  .8 > data/gr_8.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  .15 -density  .7 > data/gr_7.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .2 -density  .6 > data/gr_6.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .4 -density  .5 > data/gr_5.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  1.0 -density  .4 > data/gr_4.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  2.0 -density  .3 > data/gr_3.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  5.0 -density  .2 > data/gr_2.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size 10.0 -density  .1 > data/gr_1.csv  &
-        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size 10.0 -density .02 > data/gr_02.csv &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  .05 -density  .9 > $CHEM220_DATDIR/gr_9.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .1 -density  .8 > $CHEM220_DATDIR/gr_8.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  .15 -density  .7 > $CHEM220_DATDIR/gr_7.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .2 -density  .6 > $CHEM220_DATDIR/gr_6.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size   .4 -density  .5 > $CHEM220_DATDIR/gr_5.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  1.0 -density  .4 > $CHEM220_DATDIR/gr_4.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  2.0 -density  .3 > $CHEM220_DATDIR/gr_3.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size  5.0 -density  .2 > $CHEM220_DATDIR/gr_2.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size 10.0 -density  .1 > $CHEM220_DATDIR/gr_1.csv  &
+        ./hardsphere-gr -nsteq 500 -nstmc 5000 -step_size 10.0 -density .02 > $CHEM220_DATDIR/gr_02.csv &
     fi
 fi
 
 if [ $1 == "HW3-DATA" ]; then
-    python script/gr.py data/gr_*.csv
+    python script/gr.py $CHEM220_DATDIR/gr_*.csv
 fi
 
 
 # =============================================================================
 # HW 5
 # =============================================================================
-if [ $1 == "HW5-SIM" ]; then
-    gcc lj.cc -lstdc++ -o lj -DVERBOSE
-    if [ $? = 0 ]; then
-        ./lj
-    fi
-fi
+#if [ $1 == "HW5-SIM" ]; then
+#    gcc lj.cc -lstdc++ -o lj -DVERBOSE
+#    if [ $? = 0 ]; then
+#        ./lj
+#    fi
+#fi
